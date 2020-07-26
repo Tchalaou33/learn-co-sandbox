29b1cc2ad59dcf0bd9d6fa3910303b37ca2fab4e
Andrea Tchalaou <attchalaou@gmail.com>
2020-07-26 13:52:03 +0000

    first commit

---
+++ proj-cli/lib/proj/cli.rb
@@ -0,0 +1,8 @@
+require "proj/cli/version"
+
+module Proj
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ proj-cli/lib/proj/cli/version.rb
@@ -0,0 +1,5 @@
+module Proj
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
